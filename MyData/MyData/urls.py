509fa524c6ff292bc7d977cd326adbe542eb5e5f
Carlos Silva <carlosarturosilvaoviedo@gmail.com>
2021-02-24 12:53:48 -0500

    Creación de las APPS necesarias para el proyecto

--- MyData/MyData/urls.py
+++ MyData/MyData/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from getdata import views as getdata_views
+from users import views as users_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
