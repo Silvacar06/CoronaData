6f82954f43ee8c38917d2f7db1ac81a3e81ba9f5
Carlos Silva <carlosarturosilvaoviedo@gmail.com>
2021-02-24 14:22:48 -0500

    Conexión a base de datos y creación del modelo de usuarios con ORM django

--- MyData/MyData/settings.py
+++ MyData/MyData/settings.py
@@ -75,9 +75,17 @@ WSGI_APPLICATION = 'MyData.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mydata',
+        'USER':'postgres',
+        'PASSWORD':'admin123',
+        'HOST':'127.0.0.1',
+        'PORT':'5433'
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
 }
 
 
